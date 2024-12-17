c7664a2497067bb1aa036424cdbca2bd4b3cf5a5
mainaliashish <mainaliashish@outlook.com>
2024-12-17 17:08:29 +0000

    update: - remove commented code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <router-view />
-  <!-- <button @click="showToast">Show Success Toast</button>
-  <button @click="showErrorToast">Show Error Toast</button> -->
 </template>
 
-<script setup>
-// import { toast } from "vue3-toastify";
-// import "vue3-toastify/dist/index.css";
-
-// toast("Hello! Wow so easy!", {
-//   "theme": "auto",
-//   "type": "default",
-//   "dangerouslyHTMLString": true
-// })
-</script>
+<script setup></script>
 
 <style></style>
